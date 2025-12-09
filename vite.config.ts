f72be89b1f77c9f8b629c90b79b094e136e7d59a
Krish Zinzuvadiya <168444725+Krish-zinzuvadiya@users.noreply.github.com>
2025-12-09 21:49:58 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -11,9 +11,8 @@ export default defineConfig(({ mode }) => {
     base: "/",   // IMPORTANT for deployment
 
     define: {
-      "process.env.API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-      "process.env.GEMINI_API_KEY": JSON.stringify(env.GEMINI_API_KEY),
-    },
+  'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+},
 
     resolve: {
       alias: {
